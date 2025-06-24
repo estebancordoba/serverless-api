a8002e7ed3ca5b38df06e2ffc3a9760c1260243b
Esteban Córdoba <41124551+estebancordoba@users.noreply.github.com>
2025-06-24 08:35:46 -0500

    chore: improve deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+set -e
+
+if [ -z "$1" ]; then
+  echo "Usage: $0 <env>" >&2
+  exit 1
+fi
+
 npm run build:all
 cdk bootstrap
-cdk deploy --context env=$1
+cdk deploy --context env="$1"
